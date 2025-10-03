d375f611d131d4db5b826fb1c4bc8681dea07e05
escalanossasenhoradasgracas-gif <escalanossasenhoradasgracas@gmail.com>
2025-10-03 00:31:33 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx,jsx,js}'],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx,js,jsx}"
+  ],
+  theme: { extend: {} },
   plugins: [],
 }
